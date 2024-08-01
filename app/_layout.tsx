b937877075851cf41c79f4f54a57737daff845d8
Vinit Shirbhate <vinit.shirbhate23@vit.edu>
2024-08-02 00:33:34 +0530

    styles: authenticated tab bars created

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,11 @@ import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
-import { Link, Stack, useRouter } from "expo-router";
+import { Link, Stack, useRouter, useSegments } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import { TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 const Clerk_Publicable_Key = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
@@ -54,6 +54,7 @@ const InitialLayout = () => {
 
   const router = useRouter();
   const { isLoaded, isSignedIn } = useAuth();
+  const segmenets = useSegments();
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -68,10 +69,19 @@ const InitialLayout = () => {
 
   useEffect(() => {
     console.log("isSignedIn", isSignedIn);
+    if (!isLoaded) {
+      return;
+    }
+    const isAuthRoom = segmenets[0] === "(auth)";
+    if (isSignedIn && !isAuthRoom) {
+      router.replace("/(auth)/(tabs)/home");
+    } else if (!isSignedIn) {
+      router.replace("/");
+    }
   }, [isSignedIn]);
 
-  if (!loaded) {
-    return null;
+  if (!loaded || !isLoaded) {
+    return <Text>Loading...</Text>;
   }
 
   return (
@@ -135,6 +145,12 @@ const InitialLayout = () => {
               <Ionicons name="arrow-back" size={34} color={Colors.dark} />
             </TouchableOpacity>
           ),
+        }}
+      />
+      <Stack.Screen
+        name="(auth)/(tabs)"
+        options={{
+          headerShown: false,
         }}
       />
     </Stack>
